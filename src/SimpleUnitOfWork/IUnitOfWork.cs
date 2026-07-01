4cc0d9478c9be9edb02887ce9fa7c414eab8624e
chong <liuhong2003@live.cn>
2026-07-01 22:36:05 +0800

    refactor: 精简 IUnitOfWork 接口，移除状态属性（已抽取至 IUnitOfWorkContext）

--- src/SimpleUnitOfWork/IUnitOfWork.cs
+++ src/SimpleUnitOfWork/IUnitOfWork.cs
@@ -1,27 +1,13 @@
-﻿using System.Data;
+using System.Data;
 
 namespace SimpleUnitOfWork
 {
     /// <summary>
-    /// 工作单元接口，封装数据库连接、事务及提交/回滚操作。
+    /// 工作单元接口，管理事务的提交、回滚与创建。
+    /// 连接、事务、超时等状态属性请参见 <see cref="IUnitOfWorkContext"/>。
     /// </summary>
     public interface IUnitOfWork : IDisposable
     {
-        /// <summary>
-        /// 超时时间，单位秒，默认30秒。
-        /// </summary>
-        int CommandTimeout { get; set; }
-
-        /// <summary>
-        /// 当前活动事务，调用 Demand 后可用。
-        /// </summary>
-        IDbTransaction Transaction { get; }
-
-        /// <summary>
-        /// 当前数据库连接，工作单元负责在 Dispose 时释放。
-        /// </summary>
-        IDbConnection Connection { get; }
-
         /// <summary>
         /// 提交当前事务。
         /// </summary>
@@ -33,8 +19,9 @@ namespace SimpleUnitOfWork
         void Rollback();
 
         /// <summary>
-        /// 需求一个事务，如果当前没有事务，则创建一个新的事务。
+        /// 需求一个事务，如果当前没有事务，则根据指定隔离级别创建一个新的事务。
         /// </summary>
+        /// <param name="level">事务隔离级别</param>
         void Demand(IsolationLevel level);
     }
 }
